298f542f277e33a8d9d9c149b1b35f3357081cc9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-19 11:21:43 +0200

    Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac60b5bd40dd700349eec2707f648f46.js"
+  "/precache-manifest.2bd4e99ab2222b5a93a640f19ab3e3c6.js"
 );
 
 self.addEventListener('message', (event) => {
